0bdd7238c03b6a1db222e927a4191564e9c2f45a
r-s-kyu <83934713+r-s-kyu@users.noreply.github.com>
2021-05-20 02:10:30 +0900

    5/19の進捗＜スプレッド構文まで＞

--- src/index.js
+++ src/index.js
@@ -1,10 +1,161 @@
-import "./styles.css";
+/**
+ * const, let宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言可能
+// val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// constで定義したオジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "じゃけえ",
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.addres = "Hiroshima";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "じゃけえ";
+// const age = 28;
+// //「私の名前はじゃけえです。根鈴は28歳です。」
+
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です。"));
+
+// // アロー関数
+// // 因数が一つの時()は省略できる。しかし、prettierなどの自動整型フォーマットを入れると、()を入れられる。
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です。"));
+
+// // 関数内の処理が1行の時はreturnを省略できる。
+// // const func3 = (str) => str;
+// // console.log(func3("func3です。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// }
+// console.log(func3(10,20));
+
+// const func4 = (num1, num2) =>  num1 + num2;
+// console.log(func4(10,34));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "じゃけえ",
+//   age: 28,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const mesasage2 =  `名前は${name}です。年齢は${age}歳です。`;
+
+// const myProfile = ["じゃけえ", 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+// const [ name, age ] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// sayHello("じゃけえ");
+
+/**
+ * スプレッド構文...
+ */
+//配列の展開
+// const arr1 = [2, 3];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc =(num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+//配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = arr4 + arr5 //配列の中が文字列というカオスな状態になる
+// console.log(arr7);
+
+// // const arr7 = [...arr4, ...arr5];
+
+//参照というらしいが、以下のように配列同士を=してしまうと、arr8の変更記録がarr4にも記録されてしまう。
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
