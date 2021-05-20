a6a1325e0dcd44358694487c84596416995f1aa1
r-s-kyu <83934713+r-s-kyu@users.noreply.github.com>
2021-05-20 09:30:48 +0900

    javascriptの基本をおさらい

--- src/index.js
+++ src/index.js
@@ -133,20 +133,20 @@
 // sumFunc(...arr1);
 
 //まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
 
-//配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// //配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr6);
-console.log(arr4);
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
 
 // const arr7 = arr4 + arr5 //配列の中が文字列というカオスな状態になる
 // console.log(arr7);
@@ -159,3 +159,76 @@ console.log(arr4);
 // arr8[0] = 100;
 // console.log(arr8);
 // console.log(arr4);
+
+/**
+ * maplやfilterを使った配列の処理
+ */
+// const nameArr = ["田中", "山田", "じゃけえ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name)=> {
+//   return name;
+// })
+// console.log(nameArr);
+
+//mapの中でindexの要素を使うときは第2引数を入れる
+// nameArr.map((name, index)=> console.log(`${index + 1}番目は${name}です。`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 ===0;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけえ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// // console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください。";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています!!' : '許容範囲内です';
+// }
+// console.log(checkSum(37, 64));
+
+/**
+ * 論理演算氏の本当の意味を知ろう && //
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtureになります")
+// }
+// if (flag1 && flag2) {
+//   console.log("1か2はtureになります")
+// }
+
+// ||は左がfalseなら右側を返す。
+// const num = 100;
+// const fee = num || "金額未設定です"
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました"
+// console.log(fee2);
